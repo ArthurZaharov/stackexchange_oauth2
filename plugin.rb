2d170fa519ef167928e15106b28e4befae81fecf
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-02-06 17:51:16 +0300

    Update strategy to 2.2 version

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # version: 0.0.1
 # author: Arthur Zaharov
 
-gem 'omniauth-stackexchange', '0.2.0'
+require File.expand_path('../stackexchange-strategy.rb', __FILE__)
 
 class StackexchangeAuthenticator < ::Auth::Authenticator
   def name
